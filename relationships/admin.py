6dd554443a11f729487128e0e789e8ea0f8f1f76
Sai_Prakash <ch.saiprakash22@gmail.com>
2021-05-13 18:14:02 +0530

    relations

--- relationships/admin.py
+++ relationships/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Publication, Article, Reporter, Articler , Place, Restaurant
+
+
+# class ToDoListAdmin(admin.ModelAdmin):
+#     list_display = ['user', '__str__', 'priority','created_timestamp','modified_timestamp']
+#     form = ToDoListForm
+#     # class Meta:
+#     #     model = ToDoList
+# class ListItemAdmin(admin.ModelAdmin):
+#     list_display = ['to_do_list', '__str__', 'priority','created_timestamp','modified_timestamp']
+
+admin.site.register(Publication)
+admin.site.register(Article)
+admin.site.register(Articler)
+admin.site.register(Reporter)
+admin.site.register(Place)
+admin.site.register(Restaurant)
--- relationships/models.py
+++ relationships/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 #many-many
 class Publication(models.Model):
     title = models.CharField(max_length=30)
@@ -19,12 +20,8 @@ class Article(models.Model):
     class Meta:
         ordering = ['headline']
 
-    def __str__(self):
-        return self.headline
 
 #many to one
-
-from django.db import models
 
 class Reporter(models.Model):
     first_name = models.CharField(max_length=30)
@@ -34,7 +31,7 @@ class Reporter(models.Model):
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
 
-class Article_r(models.Model):
+class Articler(models.Model):
     headline = models.CharField(max_length=100)
     pub_date = models.DateField()
     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
@@ -42,7 +39,25 @@ class Article_r(models.Model):
     def __str__(self):
         return self.headline
 
-    class Meta:
-        ordering = ['headline']
-
 #one to one
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "%s the place" % self.name
+
+class Restaurant(models.Model):
+    place = models.OneToOneField(
+        Place,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "%s the restaurant" % self.place.name
+
