0fc963a422e3bcc28bf97ef53780874f3c7498e8
Sai_Prakash <ch.saiprakash22@gmail.com>
2021-05-17 20:46:50 +0530

    Viewsets and routers

---
+++ Rest_framework_status/api/views.py
@@ -0,0 +1,48 @@
+from rest_framework import viewsets, mixins
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from Rest_framework_status.models import Status
+from .serializers import StatusSerializer
+
+class StatusListSearchAPIView(APIView):
+    permission_classes = []
+    authentication_classes = []
+
+    def get(self, request, format= None):
+        qs = Status.objects.all()
+        serializer = StatusSerializer(qs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format = None):
+        qs = Status.objects.all()
+        serializer = StatusSerializer(qs, many=True)
+        return Response(serializer.data)
+
+
+# class StatusAPIView(APIView):
+#     permission_classes = []
+#     authentication_classes = []
+#
+#     def get(self, request, format= None):
+#         qs = Status.objects.all()
+#         serializer = StatusSerializer(qs, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format = None):
+#         qs = Status.objects.all()
+#         serializer = StatusSerializer(qs, many=True)
+#         return Response(serializer.data)
+
+class StatusAPIView(viewsets.ModelViewSet):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
+    lookup_field = 'id'
+    def get_queryset(self):
+        qs = Status.objects.all()
+        query = self.request.GET.get('q')
+        if query is not None:
+            qs = qs.filter(content__icontains=query)
+        return  qs
