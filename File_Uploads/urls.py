32d1ae9649ed2123291c32251dd6b08001fa6028
Sai_Prakash <ch.saiprakash22@gmail.com>
2021-05-20 16:11:11 +0530

    Fix File uploads via HTML form and Create Multiple file uploads

--- File_Uploads/urls.py
+++ File_Uploads/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.upload_file, name='File'),
+    path('multiple/', views.FileFieldFormView.as_view(), name='Multiple_File'),
+
 ]
