d0711d3060e191eaa53d59a6dd64bde2cc440f19
Sai_Prakash <ch.saiprakash22@gmail.com>
2021-05-27 11:57:43 +0530

    customauth app implementation 90%

--- Induction/settings.py
+++ Induction/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'File_Uploads',
     'polls',
     'Signals',
-    'Caching'
+    'Caching',
+    'customauth'
 ]
 
 MIDDLEWARE = [
---
+++ customauth/generators.py
@@ -0,0 +1,29 @@
+import secrets
+
+class BaseHashGenerator(object):
+    """
+    All generators should extend this class overriding `.hash()` method.
+    """
+
+    def hash(self):
+        raise NotImplementedError()
+
+
+class ClientSecretGenerator(BaseHashGenerator):
+    """ Generate a client_id for Basic Authentication scheme without colon char
+        as in http://tools.ietf.org/html/rfc2617#section-2"""
+
+    def hash(self):
+        length = 40
+        # chars = UNICODE_ASCII_CHARACTER_SET
+        return secrets.token_urlsafe(length)
+
+
+def generate_client_secret():
+    """
+    Generate a suitable client secret
+    """
+    client_secret_generator = ClientSecretGenerator()
+    return client_secret_generator.hash()
+
+print(generate_client_secret())
