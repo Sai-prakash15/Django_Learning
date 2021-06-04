52034ffe415bcb0904d31de9a7464c9f7a21e5bd
Sai_Prakash <ch.saiprakash22@gmail.com>
2021-06-04 16:36:33 +0530

    Clearing cache using the default cache

--- Caching/signals.py
+++ Caching/signals.py
@@ -3,6 +3,8 @@ from django.dispatch import receiver
 from .models import Recipe
 from django_redis import get_redis_connection
 from django.db import transaction
+from django.core.cache import cache
+
 
 @receiver([post_save, post_delete], sender=Recipe, dispatch_uid="clear_fancy_cache")
 def clear_fancy_cache(sender, instance, **kwargs):
@@ -10,5 +12,6 @@ def clear_fancy_cache(sender, instance, **kwargs):
     print("caching clear signal")
     if sender.__name__ in list_of_models:
         transaction.on_commit(
-            lambda: get_redis_connection("default").flushall()
+            # lambda: get_redis_connection("default").flushall()
+            lambda: cache.clear()
         )
