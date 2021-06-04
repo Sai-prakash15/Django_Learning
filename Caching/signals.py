0868c39b3bbe4a70107ce074aea7d55884b7c97b
Sai_Prakash <ch.saiprakash22@gmail.com>
2021-06-04 14:56:11 +0530

    Signlas on different senders and multiple signals on a receiver

--- Caching/signals.py
+++ Caching/signals.py
@@ -1,10 +1,14 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Recipe
 from django_redis import get_redis_connection
+from django.db import transaction
 
-
-@receiver(post_save, sender=Recipe, dispatch_uid="clear_fancy_cache")
+@receiver([post_save, post_delete], sender=Recipe, dispatch_uid="clear_fancy_cache")
 def clear_fancy_cache(sender, instance, **kwargs):
-    print("here")
-    get_redis_connection("default").flushall()
+    list_of_models = ('Recipe')
+    print("caching clear signal")
+    if sender.__name__ in list_of_models:
+        transaction.on_commit(
+            lambda: get_redis_connection("default").flushall()
+        )
